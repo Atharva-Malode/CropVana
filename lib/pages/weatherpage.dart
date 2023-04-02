459907392cf6b91c50cb27c6b4a82f7bba3db160
Atharva Malode <Atharvamalode21@gmail.com>
2023-04-02 22:30:42 +0530

    weather app update

--- lib/pages/weatherpage.dart
+++ lib/pages/weatherpage.dart
@@ -1,32 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
 
-class WeatherPage extends StatefulWidget {
+class WeatherPage extends StatelessWidget {
   const WeatherPage({super.key});
 
   @override
-  State<WeatherPage> createState() => _WeatherPageState();
-}
-
-class _WeatherPageState extends State<WeatherPage> {
-  late final WebViewController controller;
-
-  @override
-  void initState() {
-    super.initState();
-    controller = WebViewController()
-      ..loadRequest(
-        Uri.parse('https://www.google.com/search?q=weather'),
-      );
-  }
-
-  @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-      body: SafeArea(
-        child: WebViewWidget(
-          controller: controller,
-        ),
+      body: Container(
+        width: size.width,
+        height: size.height,
+        color: Colors.red,
+        child: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [],
+        )),
       ),
     );
   }
