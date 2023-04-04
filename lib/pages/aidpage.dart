df72aaf97b9af9664b77f198ac3cafa8016d4fed
Atharva Malode <Atharvamalode21@gmail.com>
2023-04-04 22:10:11 +0530

    rough stuff

--- lib/pages/aidpage.dart
+++ lib/pages/aidpage.dart
@@ -11,7 +11,8 @@ class AidPage extends StatelessWidget {
       body: SingleChildScrollView(
         child: SafeArea(
           child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal:16.0, vertical: 24.0),
+            padding:
+                const EdgeInsets.symmetric(horizontal: 16.0, vertical: 24.0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               // ignore: prefer_const_literals_to_create_immutables
@@ -25,18 +26,17 @@ class AidPage extends StatelessWidget {
                 ),
                 const Text(
                   'News for farmer',
-                  style: TextStyle(
-                    fontSize: 18.0,
-                    fontWeight: FontWeight.w500
-                  ),
+                  style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w500),
                 ),
-                const SizedBox(height: 16.0,),
+                const SizedBox(
+                  height: 16.0,
+                ),
                 TextField(
                   decoration: InputDecoration(
-                    hintText: "Search",
-                    prefixIcon: const Icon(Icons.search),
-                    border: OutlineInputBorder(borderRadius: BorderRadius.circular(12.0))
-                  ),
+                      hintText: "Search",
+                      prefixIcon: const Icon(Icons.search),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12.0))),
                 ),
                 const SizedBox(height: 20.0),
                 SingleChildScrollView(
@@ -50,117 +50,123 @@ class AidPage extends StatelessWidget {
                     ],
                   ),
                 ),
-                const SizedBox(height: 20.0,),
+                const SizedBox(
+                  height: 20.0,
+                ),
                 SingleChildScrollView(
                   child: Column(
-                  children: [
-                    _buildNewsCard(
-                      'https://cfstatic.give.do/00ae3035-de8b-44ed-8102-ce1d20a66c22.jpg',
-                      'Universal Versatile Society (UVS)',
-                      'Through our project, we are intending to make 250 families self-sustained',
-                    ),
-                    _buildNewsCard(
-                      'https://cfstatic.give.do/e40cb30b-956f-423a-a1b2-dc9db25e9347.jpg',
-                      'Dilasa Sanstha',
-                      'The NGO is sponsoring farmers to learn low-cost farming methods.',
-                    ),
-                    _buildNewsCard(
-                      'https://cfstatic.give.do/efbfa5a3-78d9-43f2-8e58-86edc14f204e.jpg',
-                      'Marathwada Navnirman Lokayat (MANAVLOK)',
-                      'The organization has developed multi yearly cropping pattern which is helpful for facing drought situation & for increasing income.',
-                    ),
-                    _buildNewsCard(
-                      'https://www.bookeventz.com/blog/wp-content/uploads/2021/02/Robin-Hood-Army.png', 
-                      'Robin Hood Army', 
-                      'The Robin Hood Army is a volunteer based, zero-funds organization that works to get surplus food from restaurants and the community to serve less fortunate people.',
-                    ),
-                    _buildNewsCard(
-                      'https://www.annakshetra.org/static/Annakshetra.png', 
-                      'Annakshetra',
-                      'The mission of NGO is to reduce food wastage.',
-                    ),
-                  ],
+                    children: [
+                      _buildNewsCard(
+                        'https://cfstatic.give.do/00ae3035-de8b-44ed-8102-ce1d20a66c22.jpg',
+                        'Universal Versatile Society (UVS)',
+                        'Through our project, we are intending to make 250 families self-sustained',
+                      ),
+                      _buildNewsCard(
+                        'https://cfstatic.give.do/e40cb30b-956f-423a-a1b2-dc9db25e9347.jpg',
+                        'Dilasa Sanstha',
+                        'The NGO is sponsoring farmers to learn low-cost farming methods.',
+                      ),
+                      _buildNewsCard(
+                        'https://cfstatic.give.do/efbfa5a3-78d9-43f2-8e58-86edc14f204e.jpg',
+                        'Marathwada Navnirman Lokayat (MANAVLOK)',
+                        'The organization has developed multi yearly cropping pattern which is helpful for facing drought situation & for increasing income.',
+                      ),
+                      _buildNewsCard(
+                        'https://www.bookeventz.com/blog/wp-content/uploads/2021/02/Robin-Hood-Army.png',
+                        'Robin Hood Army',
+                        'The Robin Hood Army is a volunteer based, zero-funds organization that works to get surplus food from restaurants and the community to serve less fortunate people.',
+                      ),
+                      _buildNewsCard(
+                        'https://www.annakshetra.org/static/Annakshetra.png',
+                        'Annakshetra',
+                        'The mission of NGO is to reduce food wastage.',
+                      ),
+                    ],
                   ),
                 ),
-              ],  
+              ],
             ),
           ),
         ),
-    ),
+      ),
     );
   }
-  Widget _buildNewsCard(String imageUrl, String title, String description) {
-  return Container(
-    padding: const EdgeInsets.all(16.0),
-    margin: const EdgeInsets.only(bottom: 16.0),
-    decoration: BoxDecoration(
-      color: Colors.white,
-      borderRadius: BorderRadius.circular(10.0),
-      boxShadow: [
-        BoxShadow(
-          color: Colors.black.withOpacity(0.2),
-          offset: const Offset(0.0, 3.0),
-          blurRadius: 6.0,
-        ),
-      ],
-    ),
-    child: Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        ClipRRect(
-          borderRadius: BorderRadius.circular(8.0),
-          child: Image.network(
-            imageUrl,
-            width: 100.0,
-            height: 100.0,
-            fit: BoxFit.cover,
-          ),
-        ),
-        const SizedBox(width: 16.0),
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                title,
-                style: const TextStyle(
-                  fontSize: 18.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 8.0),
-              Text(
-                description,
-                style: TextStyle(
-                  fontSize: 14.0,
-                  color: Colors.grey[600],
-                ),
-              ),
-            ],
-          ),
-        ),
-      ],
-    ),
-  );
-}
-Widget _buildCategoryButton(String label, bool isActive) {
-  return Container(
-    margin: const EdgeInsets.only(right: 16.0),
-    child: ElevatedButton(
-      onPressed: () {},
-      style: ButtonStyle(
-        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-          RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(16.0)
-          ),
-        ),
-        backgroundColor: isActive ? MaterialStateProperty.all<Color>(Colors.blue) : MaterialStateProperty.all<Color>(Colors.grey.shade300),
-        foregroundColor: isActive ? MaterialStateProperty.all<Color>(Colors.white) : MaterialStateProperty.all<Color>(Colors.black),
-        padding: MaterialStateProperty.all<EdgeInsetsGeometry>(const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0)),
-      ),
-      child: Text(label),
-    ),
-  );
-}
 
+  Widget _buildNewsCard(String imageUrl, String title, String description) {
+    return Container(
+      padding: const EdgeInsets.all(16.0),
+      margin: const EdgeInsets.only(bottom: 16.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10.0),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.2),
+            offset: const Offset(0.0, 3.0),
+            blurRadius: 6.0,
+          ),
+        ],
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(8.0),
+            child: Image.network(
+              imageUrl,
+              width: 100.0,
+              height: 100.0,
+              fit: BoxFit.cover,
+            ),
+          ),
+          const SizedBox(width: 16.0),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: const TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 8.0),
+                Text(
+                  description,
+                  style: TextStyle(
+                    fontSize: 14.0,
+                    color: Colors.grey[600],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildCategoryButton(String label, bool isActive) {
+    return Container(
+      margin: const EdgeInsets.only(right: 16.0),
+      child: ElevatedButton(
+        onPressed: () {},
+        style: ButtonStyle(
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(16.0)),
+          ),
+          backgroundColor: isActive
+              ? MaterialStateProperty.all<Color>(Colors.blue)
+              : MaterialStateProperty.all<Color>(Colors.grey.shade300),
+          foregroundColor: isActive
+              ? MaterialStateProperty.all<Color>(Colors.white)
+              : MaterialStateProperty.all<Color>(Colors.black),
+          padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+              const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0)),
+        ),
+        child: Text(label),
+      ),
+    );
+  }
 }
